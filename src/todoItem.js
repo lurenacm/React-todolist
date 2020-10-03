e74c1122f95fc2649be2e9f3988855094d74b69d
HWfuture <18320398437@139.com>
2020-10-03 11:16:49 +0800

    add propTypes and defaultProps valitator

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class todoItem extends Component {
     constructor(props) {
@@ -15,6 +16,17 @@ class todoItem extends Component {
         // console.log(this.props.item)
         this.props.deleteItem(this.props.index)
     }
+}
+
+// prop 类型校验
+todoItem.propTypes = {
+    item: PropTypes.arrayOf(PropTypes.string, PropTypes.number),
+    index: PropTypes.number
+}
+
+// prop 传递的默认值
+todoItem.defaultProps = {
+    item: 'hello world'
 }
 
 export default todoItem
