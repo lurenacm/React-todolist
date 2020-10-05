977972af2ec4431bf2ccef0b6ca3cce8f1468faa
HWfuture <18320398437@139.com>
2020-10-05 10:54:11 +0800

    add redux

--- src/antdTodolist.js
+++ src/antdTodolist.js
@@ -1,14 +1,28 @@
 import React, { Component, Fragment } from 'react'
+import store from './store/index'
 import 'antd/dist/antd.css'
-import { Input, Button } from 'antd' 
+import { Input, Button, List } from 'antd' 
 
 class AntdTodoList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = store.getState()
+        console.log(this.state)
+    }
+
     render() {
         return (
             <Fragment>
-                <div>
+                <div style={{marginTop: "20px"}}>
                     <Input placeholder="Basic usage" style={{width: "300px", marginRight: "10px"}} />
                     <Button type="primary">提交</Button>
+                    <List
+                        style={{ marginTop:"20px", width: "400px"}}
+                        size="small"
+                        bordered
+                        dataSource={this.state.dataList}
+                        renderItem={item => <List.Item>{item}</List.Item>}
+                        />
                 </div>
             </Fragment>
         )
---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+// 所有数据仓库
+
+const defaultStore = {
+    inputValue: '123',
+    dataList: [ 'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.']
+}
+
+export default (state= defaultStore, action) => {
+    return state
+}
